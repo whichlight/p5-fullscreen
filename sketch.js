15653bdc4374259b104eeda21f170322011f6ee3
Kawandeep Virdee <whichlight@gmail.com>
2015-08-04 00:29:11 -0400

    fix for mobile touch events

--- sketch.js
+++ sketch.js
@@ -1,17 +1,48 @@
+//touch interaction var
+var i = {x:0, y:0};
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background(0);
-  stroke(255);
-  strokeWeight(10);
-  line(windowWidth/2, 0, windowWidth/2, windowHeight);
+
+
+  //disable default touch events for mobile
+  var el = document.getElementsByTagName("canvas")[0];
+  el.addEventListener("touchstart", pdefault, false);
+  el.addEventListener("touchend", pdefault, false);
+  el.addEventListener("touchcancel", pdefault, false);
+  el.addEventListener("touchleave", pdefault, false);
+  el.addEventListener("touchmove", pdefault, false);
+}
+
+function pdefault(e){
+  e.preventDefault()
 }
 
 function draw() {
+  update();
+  render();
+}
+
+function update(){
+  //normalize interaction
+  i.x = max(touchX, mouseX);
+  i.y = max(touchY, mouseY);
+
+
+}
+
+function render(){
+  background(0);
   stroke(255);
   strokeWeight(10);
-  line(mouseX, 0, mouseX, windowHeight);
+  line(i.x, 0, i.x, height);
+
+  stroke(255);
+  strokeWeight(10);
+  line(0, i.y, width, i.y);
 }
+
 
 
 
